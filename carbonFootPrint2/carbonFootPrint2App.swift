337c203df4a6ff75f9f8092b81de0d0f946f404e
Noga <nogah0317@gmail.com>
2023-09-16 18:52:59 -0400

    Initial Commit

---
+++ carbonFootPrint2/carbonFootPrint2App.swift
@@ -0,0 +1,17 @@
+//
+//  carbonFootPrint2App.swift
+//  carbonFootPrint2
+//
+//  Created by Noga Gercsak on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct carbonFootPrint2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
